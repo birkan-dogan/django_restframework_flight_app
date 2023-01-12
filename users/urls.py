1ccbc566bb730d17f2baf6d94b915c4617b8cc3c
birkan <b.561birkan@gmail.com>
2023-01-12 18:46:20 +0300

    serializers, endpoint, view to register new user

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from .views import RegisterAPI
 
 urlpatterns = [
     path('auth/', include('dj_rest_auth.urls')),  # https://domain/users/auth/
+    path("register/", RegisterAPI.as_view()),
 ]
