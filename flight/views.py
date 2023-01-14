7f6db98ca70ad29c155d9462f7d082dde4cd5c9e
birkan <b.561birkan@gmail.com>
2023-01-14 18:46:09 +0300

    overriding queryset and serializer_class methods to get filtered data and specific data for admin or regular users

--- flight/views.py
+++ flight/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from .models import Flight, Reservation
-from .serializers import FlightSerializer, ReservationSerializer
+from .serializers import FlightSerializer, ReservationSerializer, StaffSerializer
 from rest_framework.permissions import IsAdminUser
 from .permissions import IsStafforReadOnly
 
@@ -11,7 +11,24 @@ class FlightView(viewsets.ModelViewSet):
     serializer_class = FlightSerializer
     permission_classes = (IsStafforReadOnly, )
 
+    # personel, uçuş bilgilerinin yanında rezervasyonları da görsün istiyoruz ve bunu gerçekleştirmek için get_serializer_class() methodunu override ediyoruz.
+    def get_serializer_class(self):
+        serializer = super().get_serializer_class()
+        if(self.request.user.is_staff):
+            return StaffSerializer
+        return serializer
+
 
 class ReservationView(viewsets.ModelViewSet):
     queryset = Reservation.objects.all()
     serializer_class = ReservationSerializer
+
+    # kullanıcı sadece kendi rezervasyonlarını listelesin istiyoruz (filtreleme). Bunun için get_queryset methodunu override ediyoruz
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        if(self.request.user.is_staff):
+            return queryset
+
+        return queryset.filter(user = self.request.user)
